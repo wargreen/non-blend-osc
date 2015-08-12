829e7b5cf97620ab8622045c96084734f8fd4686
Wargreen <wargreen@lebib.org>
2015-08-13 00:20:27 +0200

    adding config file

---
+++ Blender/imports/config.py
@@ -0,0 +1,9 @@
+conf = {
+	"osc_client_addr" : "127.0.0.1",
+	"osc_client_port" : 1337,
+	# full OSC path = osc_base_path + ObjectName + osc_[angle, radius, elevation]_path
+	"osc_base_path": "/strip/",
+	"osc_angle_path": "/Spatializer/angle",
+	"osc_radius_path": "/Spatializer/radius",
+	"osc_elevation_path": "/Spatializer/elev"
+	}
